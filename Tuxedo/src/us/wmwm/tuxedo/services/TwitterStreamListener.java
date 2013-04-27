8f3b1694814bd24381456d164cda2c6133adb7db
Gravener <gravener@BAM-024170-RG.(none)>
2013-04-27 15:58:41 -0400

    let there be chat heads

--- Tuxedo/src/us/wmwm/tuxedo/services/TwitterStreamListener.java
+++ Tuxedo/src/us/wmwm/tuxedo/services/TwitterStreamListener.java
@@ -15,44 +15,47 @@ import android.graphics.PixelFormat;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
-
-import com.loopj.android.image.SmartImageView;
 
 public class TwitterStreamListener implements UserStreamListener {
 
 	AdvancedTwitterService service;
 	TwitterStream stream;
 	WindowManager windowManager;
-	
+
 	Handler handler = new Handler();
-	
-	public TwitterStreamListener(TwitterStream stream, AdvancedTwitterService service) {
+
+	public TwitterStreamListener(TwitterStream stream,
+			AdvancedTwitterService service) {
 		this.service = service;
 		this.stream = stream;
-		this.windowManager = (WindowManager) service.getSystemService(Context.WINDOW_SERVICE);
+		this.windowManager = (WindowManager) service
+				.getSystemService(Context.WINDOW_SERVICE);
 	}
 
 	@Override
 	public void onDeletionNotice(StatusDeletionNotice del) {
-		service.getApp().getSession().getStatusDAO().deleteStatus(del.getStatusId());
+		service.getApp().getSession().getStatusDAO()
+				.deleteStatus(del.getStatusId());
 		Intent i = new Intent();
 		i.setAction(AdvancedTwitterService.STATUS_DELETED);
 		i.putExtra("dn", del);
 		service.sendBroadcast(i);
-		
+
 	}
 
 	@Override
 	public void onScrubGeo(long arg0, long arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onStallWarning(StallWarning arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -60,25 +63,52 @@ public class TwitterStreamListener implements UserStreamListener {
 		handler.post(new Runnable() {
 			@Override
 			public void run() {
-				TwitterHead chatHead = new TwitterHead(service);
+				final TwitterHead chatHead = new TwitterHead(service);
 				chatHead.update(status);
-				Log.i("PROFILE_IMAGE", status.getUser().getOriginalProfileImageURL());
-				 WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-					        WindowManager.LayoutParams.WRAP_CONTENT,
-					        WindowManager.LayoutParams.WRAP_CONTENT,
-					        WindowManager.LayoutParams.TYPE_PHONE,
-					        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
-					        PixelFormat.TRANSLUCENT);
+				Log.i("PROFILE_IMAGE", status.getUser()
+						.getOriginalProfileImageURL());
+				final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+						WindowManager.LayoutParams.WRAP_CONTENT,
+						WindowManager.LayoutParams.WRAP_CONTENT,
+						WindowManager.LayoutParams.TYPE_PHONE,
+						 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+						PixelFormat.RGBA_8888);
 
-					    params.gravity = Gravity.NO_GRAVITY;
-					    params.x = 0;
-					    params.y = 100;
-					    windowManager.addView(chatHead, params);
+				params.gravity = Gravity.NO_GRAVITY;
+				params.x = 0;
+				params.y = 100;
+				chatHead.setOnTouchListener(new View.OnTouchListener() {
+					  private int initialX;
+					  private int initialY;
+					  private float initialTouchX;
+					  private float initialTouchY;
+
+					  @Override public boolean onTouch(View v, MotionEvent event) {
+					    switch (event.getAction()) {
+					      case MotionEvent.ACTION_DOWN:
+					        initialX = params.x;
+					        initialY = params.y;
+					        initialTouchX = event.getRawX();
+					        initialTouchY = event.getRawY();
+					        return true;
+					      case MotionEvent.ACTION_UP:
+					        return true;
+					      case MotionEvent.ACTION_MOVE:
+					        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+					        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+					        windowManager.updateViewLayout(chatHead, params);
+					        return true;
+					    }
+					    return false;
+					  }
+					});
+				windowManager.addView(chatHead, params);
 			}
 		});
-	
+
 		try {
-			service.getApp().getSession().getStatusDAO().saveStatus(stream.getId(), status);
+			service.getApp().getSession().getStatusDAO()
+					.saveStatus(stream.getId(), status);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -90,122 +120,122 @@ public class TwitterStreamListener implements UserStreamListener {
 			service.updateStatusNotification(stream.getId(), status);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
-		} 
-		
-		
+		}
+
 	}
 
 	@Override
 	public void onTrackLimitationNotice(int arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onException(Exception arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onBlock(User arg0, User arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onDeletionNotice(long arg0, long arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onDirectMessage(DirectMessage dm) {
 		// TODO Auto-generated method stub
-		service.getApp().getSession().getDirectMessageDAO().saveDirectMessage(dm);
+		service.getApp().getSession().getDirectMessageDAO()
+				.saveDirectMessage(dm);
 		Intent i = new Intent();
 		i.setAction(AdvancedTwitterService.DM_STREAMED);
 		i.putExtra("dm", dm);
 		service.sendBroadcast(i);
-		
+
 	}
 
 	@Override
 	public void onFavorite(User arg0, User arg1, Status arg2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onFollow(User arg0, User arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onFriendList(long[] arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onUnblock(User arg0, User arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onUnfavorite(User arg0, User arg1, Status arg2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onUserListCreation(User arg0, UserList arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onUserListDeletion(User arg0, UserList arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onUserListMemberAddition(User arg0, User arg1, UserList arg2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onUserListMemberDeletion(User arg0, User arg1, UserList arg2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onUserListSubscription(User arg0, User arg1, UserList arg2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onUserListUnsubscription(User arg0, User arg1, UserList arg2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onUserListUpdate(User arg0, UserList arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onUserProfileUpdate(User arg0) {
 		// TODO Auto-generated method stub
-		
+
 	};
-	
+
 }
--- Tuxedo/src/us/wmwm/tuxedo/views/TwitterHead.java
+++ Tuxedo/src/us/wmwm/tuxedo/views/TwitterHead.java
@@ -4,6 +4,8 @@ import twitter4j.Status;
 import us.wmwm.tuxedo.R;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.loopj.android.image.SmartImageView;
@@ -11,13 +13,13 @@ import com.loopj.android.image.SmartImageView;
 public class TwitterHead extends RelativeLayout {
 
 	SmartImageView icon;
-	
+
 	public TwitterHead(Context ctx) {
 		super(ctx);
 		LayoutInflater.from(ctx).inflate(R.layout.twitter_head, this);
 		icon = (SmartImageView) findViewById(R.id.icon);
 	}
-	
+
 	public void update(Status status) {
 		icon.setImageUrl(status.getUser().getBiggerProfileImageURL());
 	}
